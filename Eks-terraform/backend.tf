4b0dc799dec20613a01b9e0012833b59fcd6809a
aarizsak <120405944+aarizsak@users.noreply.github.com>
2025-05-27 20:55:29 +0100

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "argocd-bucket-ali" # Replace with your actual S3 bucket name
     key    = "eks/terraform.tfstate"
-    region = "us-west-2"
+    region = "us-east-1"
   }
 }
